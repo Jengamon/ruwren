0ee72ebb90506fde9314d7c5fadecff8a8d67e95
Jengamon <bob.hostern@gmail.com>
2020-05-31 08:45:05 -0400

    Make send_foreign! actually work properly
    - Also now uses a readable type name for error message

--- src/lib.rs
+++ src/lib.rs
@@ -464,15 +464,17 @@ macro_rules! get_slot_checked {
     };
 }
 
+pub fn type_name_of<T>(_: T) -> &'static str {
+    any::type_name::<T>()
+}
+
 /// Sends a foreign object [$obj] as an object of [$class] in module [$modl] to slot [$slot]
 #[macro_export]
 macro_rules! send_foreign {
     ($vm:expr, $modl:expr, $class:expr, $obj:expr => $slot:expr) => {
         {
-            use std::any::Any;
-
-            if !$vm.set_slot_new_foreign($modl, $class, $obj, $slot).is_none() {
-                panic!("rust error [{}:{}]: Could not send type {:?} as [{}] {}", file!(), line!(), $obj.type_id(), $modl, $class);
+            if $vm.set_slot_new_foreign($modl, $class, $obj, $slot).is_none() {
+                panic!("rust error [{}:{}]: Could not send type {:?} as [{}] {}", file!(), line!(), $crate::type_name_of($obj), $modl, $class);
             }
         }
     }
