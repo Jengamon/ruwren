34942298b1a6edb2bd92aa23057279fa538602c4
knh190 <knh190@hotmail.com>
2021-02-25 13:16:54 -0500

    Add basic file loader.

--- examples/relative_imports.rs
+++ examples/relative_imports.rs
@@ -1,31 +1,20 @@
-use ruwren::{VMConfig, ModuleScriptLoader};
-
-struct TestLoader;
-
-impl ModuleScriptLoader for TestLoader {
-	fn load_script(&mut self, module: String) -> Option<String> {
-		if module == "test/a" {
-			Some(include_str!("relative_imports/a.wren").into())
-		} else if module == "test" {
-			Some(include_str!("relative_imports/test.wren").into())
-		} else {
-			None
-		}
-	}
-}
+use ruwren::{BasicFileLoader, VMConfig};
 
 fn main() {
-	let vm = VMConfig::new()
-		.enable_relative_import(true)
-		.script_loader(TestLoader)
-		.build();
+    let mut script_loader = BasicFileLoader::new();
+    script_loader.base_dir("examples/relative_imports");
 
-	let main_script = include_str!("relative_imports/main.wren");
-	match vm.interpret("main", main_script) {
-		Ok(_) => {},
-		Err(e) => {
-			eprintln!("{}", e);
-			panic!("Unexpected error!");
-		}
-	}
+    let vm = VMConfig::new()
+        .enable_relative_import(true)
+        .script_loader(script_loader)
+        .build();
+
+    let main_script = include_str!("relative_imports/main.wren");
+    match vm.interpret("main", main_script) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("{}", e);
+            panic!("Unexpected error!");
+        }
+    }
 }
